428a2cdfc347eac3266efbe4bc2992bc31bc4e8a
Thiago-Reis-Porto <42750555+Thiago-Reis-Porto@users.noreply.github.com>
2022-10-24 23:40:21 -0300

    Update fullsearch.c

--- src/fullsearch.c
+++ src/fullsearch.c
@@ -7,14 +7,19 @@
 #define WIDTH 640
 #define HEIGHT 360
 #define N_FRAMES 120
-#define BLOCK_W 8
 #define BLOCK_H 5
-#define LUMA_SIZE (WIDTH * HEIGHT)
-#define CHROMA_SIZE (LUMA_SIZE / 2)
-#define FRAME_SIZE (LUMA_SIZE + CHROMA_SIZE)
-#define MAX_H (HEIGHT - BLOCK_H + 1)
-#define MAX_W (WIDTH - BLOCK_W + 1)
-#define N_BLOCKS (LUMA_SIZE / (BLOCK_W*BLOCK_H))
+#define BLOCK_W 8
+#define SEARCH_AREA_S 32
+
+enum frame_cfg
+{
+    LUMA_SIZE = WIDTH * HEIGHT,
+    CHROMA_SIZE = LUMA_SIZE / 2,
+    FRAME_SIZE = LUMA_SIZE + CHROMA_SIZE,
+    MAX_H = HEIGHT - BLOCK_H + 1,
+    MAX_W = WIDTH - BLOCK_W + 1,
+    N_BLOCKS = LUMA_SIZE / (BLOCK_H*BLOCK_W)
+};
 
 //Keep only first frame and the motion vectors(from block matching).
 struct video_compacted
@@ -83,9 +88,52 @@ struct frames *load_file(char *file_name)
     return frames;
 }
 
+int *get_search_area_pos(int x, int y)
+{
+    /*
+        Get an Search Area centered around an block.
 
+        args:
+            x - int - column position of the block
+            y - int - line position of the block
 
-int *block_matching(int *block_pos, unsigned char (*frame_R)[WIDTH], unsigned char (*frame_A)[WIDTH])
+        return:
+            best_pos - int* - array with x,y position of best matching block
+
+    */
+    int area_x, area_y;
+    int bound_h = (SEARCH_AREA_S - BLOCK_H) / 2;
+    int bound_w = (SEARCH_AREA_S - BLOCK_W) / 2;
+
+    // Vertical bounds
+    area_y = y - bound_h;
+    if (area_y < 0)
+        area_y = 0;
+    else
+    {
+        int lower_bound = y + BLOCK_H - 1 + bound_h;
+        if (lower_bound > HEIGHT - 1)
+            area_y -= lower_bound - HEIGHT;
+    }
+
+    // Horizontal bounds
+    area_x = x - bound_w;
+    if (area_x < 0)
+        area_x = 0;
+    else
+    {
+        int right_bound = x + BLOCK_W - 1 + bound_w;
+        if (right_bound > WIDTH - 1)
+            area_x -= right_bound - WIDTH;
+    }
+
+    int *search_area_pos = malloc(2 * sizeof(int));
+    search_area_pos[0] = area_x;
+    search_area_pos[1] = area_y;
+    return search_area_pos;
+}
+
+int *block_matching(int *block_pos, int *area_pos, unsigned char (*frame_R)[WIDTH], unsigned char (*frame_A)[WIDTH])
 {
     /*
     Return the position of the best matching block inside an Search Area.
@@ -103,37 +151,40 @@ int *block_matching(int *block_pos, unsigned char (*frame_R)[WIDTH], unsigned ch
     int *best_pos = malloc(2 * sizeof(int));
     int i, j, k, l;
     int block_x, block_y;
+    int area_x, area_y;
 
     block_x = block_pos[0];
     block_y = block_pos[1];
-   
-    #pragma omp for collapse(2) schedule(static, 180)
-    for (i = 0; i < MAX_H; ++i)
+    area_x = area_pos[0];
+    area_y = area_pos[1];
+
+ // #pragma omp parallel for collapse(2) schedule(dynamic, SEARCH_AREA_S*SEARCH_AREA_S/16)
+    for (i = 0; i < SEARCH_AREA_S; ++i)
     {
-        for (j = 0; j < MAX_W; ++j)
+        for (j = 0; j < SEARCH_AREA_S; ++j)
         {
             SAD = 0;
             for (k = 0; k < BLOCK_H; ++k)
             {
                 for (l = 0; l < BLOCK_W; ++l)
                 {
-                    SAD += abs(frame_R[i + k][j + l] - frame_A[block_x + k][block_y + l]);
+                    SAD += abs(frame_R[area_y + i + k][area_x + j + l] - frame_A[block_y + k][block_x + l]);
                 }
             }
-            
+
             #pragma omp critical 
             if (SAD < best_SAD)
             {
                 best_SAD = SAD;
-                best_pos[0] = i;
-                best_pos[1] = j;
+                best_pos[0] = j; //x
+                best_pos[1] = i; //y
             }
+            
         }
     }
     
     return best_pos;
 }
-
 
 struct pos *full_search(unsigned char (*frame_R)[WIDTH], unsigned char (*frame_A)[WIDTH])
 {
@@ -151,23 +202,27 @@ struct pos *full_search(unsigned char (*frame_R)[WIDTH], unsigned char (*frame_A
     int i, j;
     int block_pos[2];
     int *best_pos;
+    int block_idx;
+    int b_width = WIDTH/BLOCK_W;
+    int *area_pos;
 
     struct pos *motion_vectors = malloc(N_BLOCKS * sizeof(struct pos));
     
-    //#pragma omp for collapse(2) schedule(static, 180)
+   #pragma omp parallel for collapse(2) //schedule(dynamic, (MAX_H*MAX_W)/4 )
     for(i = 0; i < HEIGHT; i+=BLOCK_H)
     {
         for (j = 0; j < WIDTH; j+=BLOCK_W)
         {
-            block_pos[0] = j;
-            block_pos[1] = i;
-            best_pos = block_matching(block_pos, frame_R, frame_A);
-            motion_vectors[i*BLOCK_W + j].x = best_pos[0];
-            motion_vectors[i*BLOCK_W + j].y = best_pos[1];
+            block_idx = (i/BLOCK_H)* b_width + (j/BLOCK_W);
+            block_pos[0] = j; //x
+            block_pos[1] = i; //y
+            area_pos = get_search_area_pos(block_pos[0], block_pos[1]);
+            best_pos = block_matching(block_pos, area_pos, frame_R, frame_A);
+            motion_vectors[block_idx].x = best_pos[0];
+            motion_vectors[block_idx].y = best_pos[1];
             free(best_pos);
-            
+            free(area_pos);
         }
-       printf("\nLinha: %d\n", i);
     }
     
     return motion_vectors;
@@ -193,29 +248,30 @@ int main()
     memcpy(video_compacted.luma, frames->luma[0], sizeof(char) * LUMA_SIZE);
     memcpy(video_compacted.chroma, frames->chroma[0], sizeof(char) * CHROMA_SIZE);
     
-    for (i = 1; i < N_FRAMES; ++i)
-    {
-        frame_A = (unsigned char(*)[WIDTH])frames->luma[i];
-        video_compacted.motion_vectors[i - 1] = full_search(frame_R, frame_A);
-        
-        if (i%20 == 0)
+    #pragma omp parallel
+    {   
+       #pragma omp single 
         {
-            printf("\nFRAMES PROCESSED: %d\n", i);
+            for (i = 1; i < N_FRAMES; ++i)
+            {   
+                frame_A = (unsigned char(*)[WIDTH])frames->luma[i];
+                video_compacted.motion_vectors[i - 1] = full_search(frame_R, frame_A);
+                if (i%20 == 0)
+                {
+                    printf("\nFRAMES PROCESSED: %d\n", i);
+                }
+            }
         }
-        
-        
     }
-    
-    printf("\nFRAMES PROCESSED: %d\n", i);
+
+    printf("\nALL FRAMES PROCESSED\n");
     
     clock_t end = clock();
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-
     printf("The elapsed time is %f seconds \n", time_spent);
     
-    for (i=0; i<N_BLOCKS-1; ++i)
+    for (i=0; i<N_FRAMES-1; ++i)
         free(video_compacted.motion_vectors[i]);
        
-
     return 0;
 }
