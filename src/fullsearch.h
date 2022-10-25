9ee4d7bef5d9d4167529dae04b14f17a7c713078
HuberM1998 <huber.mathaus@gmail.com>
2022-10-25 17:04:43 -0300

    Cabeçalho

--- src/fullsearch.h
+++ src/fullsearch.h
@@ -1,14 +1,19 @@
 #define WIDTH 640
 #define HEIGHT 360
 #define N_FRAMES 120
-#define BLOCK_W 8
 #define BLOCK_H 5
-#define LUMA_SIZE (WIDTH * HEIGHT)
-#define CHROMA_SIZE (LUMA_SIZE / 2)
-#define FRAME_SIZE (LUMA_SIZE + CHROMA_SIZE)
-#define MAX_H (HEIGHT - BLOCK_H + 1)
-#define MAX_W (WIDTH - BLOCK_W + 1)
-#define N_BLOCKS (LUMA_SIZE / (BLOCK_W*BLOCK_H))
+#define BLOCK_W 8
+#define SEARCH_AREA_S 32
+
+enum frame_cfg
+{
+    LUMA_SIZE = WIDTH * HEIGHT,
+    CHROMA_SIZE = LUMA_SIZE / 2,
+    FRAME_SIZE = LUMA_SIZE + CHROMA_SIZE,
+    MAX_H = HEIGHT - BLOCK_H + 1,
+    MAX_W = WIDTH - BLOCK_W + 1,
+    N_BLOCKS = LUMA_SIZE / (BLOCK_H*BLOCK_W)
+};
 
 //Keep only first frame and the motion vectors(from block matching).
 struct video_compacted
@@ -32,5 +37,7 @@ struct frames
 
 struct frames *alloc_frames();
 struct frames *load_file(char *file_name);
-int *block_matching(int *block_pos, unsigned char (*frame_R)[WIDTH], unsigned char (*frame_A)[WIDTH]);
+int *get_search_area_pos(int x, int y);
+int *block_matching(int *block_pos, int *area_pos, unsigned char (*frame_R)[WIDTH], unsigned char (*frame_A)[WIDTH]);
 struct pos *full_search(unsigned char (*frame_R)[WIDTH], unsigned char (*frame_A)[WIDTH]);
+int write_file_out(float time);
